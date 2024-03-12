b8601c0b6e0e0cb0401e9d2d98070ef32f5666d9
Christian <cetischristianocana@gmail.com>
2024-03-12 15:57:31 -0600

    Diseño base iniciado

---
+++ Templates/js/main.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const menuBtn = document.querySelector(".menu-btn");
+    const navbar = document.querySelector(".navbar");
+
+    menuBtn.addEventListener("click", function () {
+        navbar.classList.toggle("active");
+    });
+});
